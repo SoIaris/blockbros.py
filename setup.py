bcd3f71fed7ed136456630d4391a54cb849c71ba
SoIaris <freemovesbycylix+1@gmail.com>
2026-02-10 14:39:00 -0500

    forgot to change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='blockbros_py',
-    version='1.2.1',
+    version='1.3.1',
     packages=find_packages(),
     install_requires=[
         "requests"
